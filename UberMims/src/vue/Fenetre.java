d854f62c01cb7a5569575d4d9f5e14375ba213e4
Antony Martin <amartin5@insa-lyon.fr>
2019-11-18 11:55:59 +0100

    Add some graphics

--- UberMims/src/vue/Fenetre.java
+++ UberMims/src/vue/Fenetre.java
@@ -1,6 +1,11 @@
 package vue;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Fenetre extends JFrame{
 
@@ -8,7 +13,32 @@ public class Fenetre extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Color backgroundColor = new Color(191,252,251);
 
 	
+	private JButton boutonChargement = new JButton("Charger le plan de la ville" );
 
+	public Fenetre(){
+		this.setTitle("Accueil UberMims");
+	    this.setSize(1200, 800);
+	    this.setLocationRelativeTo(null);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	    
+	    //Instanciation d'un objet JPanel
+	    JPanel pan = new JPanel();
+	    //Définition de sa couleur de fond
+	    pan.setLayout(null);
+	    pan.setBackground(backgroundColor);
+	    pan.setSize(1200, 800);
+	    boutonChargement.setBounds(400,350,400,100);
+	    
+	    pan.add(boutonChargement);
+	    this.setContentPane(pan);    
+	    this.setVisible(true);
+	    this.setResizable(false);
+	}
+	
+	public static void main(String[] args){       
+	    Fenetre fen = new Fenetre();
+	}       
 }
