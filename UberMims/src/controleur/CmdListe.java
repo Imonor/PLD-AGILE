db75bc805a9899b8f69f3ab5cf69868d510a6b6c
DevMax <maximewoirin.97@orange.fr>
2019-11-25 09:25:48 +0100

    New class CmdListe.java

---
+++ UberMims/src/controleur/CmdListe.java
@@ -0,0 +1,28 @@
+package controleur;
+
+import java.util.LinkedList;
+
+public class CmdListe {
+
+	private LinkedList<Commande>liste;
+	private int i;
+	
+	public CmdListe () {
+		liste = new LinkedList<Commande>();
+		i = -1;
+	}
+	
+	public void addCommande(Commande commande) {
+		liste.add(commande);
+		i = liste.size()-1;
+	}
+	
+	public void undo() {
+		if(i>=0)
+			liste.get(i--).undoCode();
+	}
+	public void redo() {
+		if(i<liste.size()-1)
+			liste.get(++i).doCode();
+	}	
+}
