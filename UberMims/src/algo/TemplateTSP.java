600060c53c8eab37065f627d4cabb41b214f6aec
robertcinciuc <robertcinciuc@gmail.com>
2019-12-04 12:07:54 +0100

    add noeudsUpdate, ordreNoeuds et interactions

--- UberMims/src/algo/TemplateTSP.java
+++ UberMims/src/algo/TemplateTSP.java
@@ -69,19 +69,7 @@ public abstract class TemplateTSP implements TSP{
 		}
 		
 		//Initialisation de la HashMap vuDispo - contenant les attributs boolean vu et dispo
-		HashMap<String, Paire> vuDispo = new HashMap<String, Paire>();
-		
-		for (HashMap.Entry<String, Intersection> iterator : intersections.entrySet()) {
-		    if( iterator.getValue() instanceof PointEnlevement ) {
-		    	vuDispo.put( iterator.getKey(), new Paire(true, false) );
-		    }else {
-		    	vuDispo.put( iterator.getKey(), new Paire(false, false) );
-		    }
-		}
-		
-		//Mettre premier noeud comme deja visite - ici le premier est l'entrepot
-		vuDispo.get(contraintes.getDepot().getId()).setDispo(false);
-		vuDispo.get(contraintes.getDepot().getId()).setVu(true);
+		HashMap<String, Paire> vuDispo = initVuDispo(contraintes, intersections);
 		
 		Tournee tournee = new Tournee();
 		//Sequentiel et MinFirst
@@ -141,17 +129,31 @@ public abstract class TemplateTSP implements TSP{
 		Iterator<Chemin> it = tournee.getPlusCourteTournee().iterator();
 		Iterator<Chemin> it2 = tournee.getPlusCourteTournee().iterator();
 		int nbChemins = tournee.getPlusCourteTournee().size();
-		int i = 0 ;
 		tempsLimiteAtteint = false;
 		Tournee tourneeReponse = new Tournee(tournee.getPlusCourteTournee(), tournee.getContraintes());
 		
+		//Initialisation nouvelle map ordreNoeuds pour le 2-opt - ordre dans la tournee qu'on calcule
+		//Pour chaque chemin c'est le noeuds de depart qui est mis ici
+		HashMap<String, Integer> ordreNoeuds = new HashMap<String, Integer>();
+		for (HashMap.Entry<String, Intersection> iterator : intersections.entrySet()) {
+	    	ordreNoeuds.put( iterator.getKey(), -1 );
+		}
+		
 		//Algo 2-Opt
+		int i = 0 ;
 		while(it.hasNext() && i<(nbChemins - 2)) {
 			Chemin chemin = it.next();
-			int j = 0 ;
+			String depart1 = chemin.getPremiere().getId();
+			String fin1 = chemin.getDerniere().getId();
+			ordreNoeuds.replace(chemin.getPremiere().getId(), i);
 			
+			ArrayList<String> noeudsUpdate = new ArrayList<String>();
+			
+			int j = 0 ;
 			while(it2.hasNext() && j<nbChemins) {
 				Chemin chemin2 = it2.next();
+				ordreNoeuds.replace(chemin2.getPremiere().getId(), j);
+				noeudsUpdate.add(chemin2.getPremiere().getId());
 				
 				//Pour ne pas depasser le temps limite
 				if (System.currentTimeMillis() - tpsDebut > tpsLimite){
@@ -160,51 +162,61 @@ public abstract class TemplateTSP implements TSP{
 				}
 				
 				if(j>i+2) {
-					int dureeIni = chemin.getDuree() + chemin2.getDuree();
-					String depart1Depart2 = chemin.getPremiere().getId() + chemin2.getPremiere().getId();
-					String fin1Fin2 = chemin.getDerniere().getId() + chemin2.getDerniere().getId();
-					int index1 = tournee.getPlusCourteTournee().indexOf(chemin);
-					int index2 = tournee.getPlusCourteTournee().indexOf(chemin2);
-					
-					//Dans le cas ou le point de depart du deuxieme chemin du swap est une livraison
-					//il faut s'assurer que son pt d'enlevement a ete vu avant le point de depart
-					//du premier chemin du swap pour pouvoir faire le swap
-					boolean enlevVu = false;
 					String depart2 = chemin2.getPremiere().getId();
-					if( intersections.get(depart2) instanceof PointLivraison ) {
-						
-						String enlev = ((PointLivraison)intersections.get(depart2)).getIdEnlevement();
-						int posEnlev = -1;
-						boolean end = false;
-						for(int count=0 ; count<index2 && !end; ++count) {
-							if(tournee.getPlusCourteTournee().get(count).getDerniere().equals(enlev)) {
-								posEnlev = count;
-								end = true;
+					String fin2 = chemin2.getDerniere().getId();
+					int dureeIni = chemin.getDuree() + chemin2.getDuree();
+					
+					//On met l'ordre inverse aux noeuds qu'on va inverser pour tester les precedences
+					for(int count = 0; count<noeudsUpdate.size(); ++count) {
+						ordreNoeuds.replace(noeudsUpdate.get(count), j+(noeudsUpdate.size()-count));
+					}
+
+					//Pour tous les chemins qu'on va mettre dans le sens inverse, on s'assure
+					//que pour tous les points de livraison, leur pt d'enlevement est vu avant
+					//ou au noeds de depart du premier chemin du swap
+					boolean possible = true;
+					for(int count = noeudsUpdate.size()-1; count>0; --count) {
+						if( intersections.get(noeudsUpdate.get(count)) instanceof PointLivraison ) {
+							String sonPtEnlev = ((PointLivraison)intersections.get(noeudsUpdate.get(count))).getIdEnlevement();
+							if( ordreNoeuds.get(sonPtEnlev) < ordreNoeuds.get(noeudsUpdate.get(count)) ) {
+								possible = false;
 							}
 						}
-						
-						if(posEnlev<=index1) {
-							enlevVu = true;
-						}
 					}
 					
-					if( (couts.get(depart1Depart2) + couts.get(fin1Fin2)) < dureeIni && enlevVu) {
-						//Trouver les chemins extremes + on met les 2 chemins extremes dans la tournee dans l'ordre change
-						Chemin newChemin1 = plusCourtsChemins.get(chemin.getPremiere().getId()).get(chemin2.getPremiere().getId());
-						Chemin newChemin2 = plusCourtsChemins.get(chemin.getDerniere().getId()).get(chemin2.getDerniere().getId());
-						tournee.getPlusCourteTournee().set(index1, newChemin1);
-						tournee.getPlusCourteTournee().set(index2, newChemin2);
-						
-						tournee = twoOptSwap(index1, index2, tournee, plusCourtsChemins);
+					//Calculer la duree du nouvel ensemble de chemins inverses
+					int newDuree = 0;
+					for(int count = noeudsUpdate.size(); count>1; ++count) {
+						newDuree += couts.get(noeudsUpdate.get(count)+noeudsUpdate.get(count-1));
 					}
+					newDuree = couts.get(noeudsUpdate.get(i)+noeudsUpdate.get(0));
+					newDuree = couts.get(noeudsUpdate.get(noeudsUpdate.size())+noeudsUpdate.get(chemin2.getDerniere().getId()));
+					
+					
+					//Il faut calculer le couts de l'ensemble de chemins recalcules et le comparer a celui existant
+//					if( (couts.get(depart1+depart2) + couts.get(fin1+fin2)) < dureeIni && enlevVu) {
+//						//Trouver les chemins extremes + on met les 2 chemins extremes dans la tournee dans l'ordre change
+//						Chemin newChemin1 = plusCourtsChemins.get(chemin.getPremiere().getId()).get(chemin2.getPremiere().getId());
+//						Chemin newChemin2 = plusCourtsChemins.get(chemin.getDerniere().getId()).get(chemin2.getDerniere().getId());
+//						tournee.getPlusCourteTournee().set(index1, newChemin1);
+//						tournee.getPlusCourteTournee().set(index2, newChemin2);
+//						
+//						tournee = twoOptSwap(index1, index2, tournee, plusCourtsChemins);
+//					}
 				}
 				++j;
 			}
+			
+			//Remet les noeuds qu'on a visite dans la 2eme boucle a l'etat non vus
+			for(int count = 0; count<noeudsUpdate.size(); ++count) {
+				ordreNoeuds.replace( noeudsUpdate.get(count), -1 );
+			}
+			
 			++i;
 		}
 	}
 	
-	public Tournee twoOptSwap(int index1, int index2, Tournee tournee, Map<String, Map<String, Chemin>> plusCourtsChemins ) {
+	private Tournee twoOptSwap(int index1, int index2, Tournee tournee, Map<String, Map<String, Chemin>> plusCourtsChemins ) {
 		
 		//On mets les chemins compris entre index1 et index2 dans l'ordre inverse de parcours pour connecter la tournee
 		Tournee reponse = new Tournee(tournee.getPlusCourteTournee(), tournee.getContraintes());
@@ -217,7 +229,24 @@ public abstract class TemplateTSP implements TSP{
 		}
 		
 		return reponse;
-	}	
+	}
+	
+	private HashMap<String, Paire> initVuDispo(ContraintesTournee contraintes, HashMap<String, Intersection> intersections){
+		HashMap<String, Paire> vuDispo = new HashMap<String, Paire>();
+		for (HashMap.Entry<String, Intersection> iterator : intersections.entrySet()) {
+		    if( iterator.getValue() instanceof PointEnlevement ) {
+		    	vuDispo.put( iterator.getKey(), new Paire(true, false) );
+		    }else {
+		    	vuDispo.put( iterator.getKey(), new Paire(false, false) );
+		    }
+		}
+		
+		//Mettre premier noeud comme deja visite - ici le premier est l'entrepot
+		vuDispo.get(contraintes.getDepot().getId()).setDispo(false);
+		vuDispo.get(contraintes.getDepot().getId()).setVu(true);
+		
+		return vuDispo;
+	}
 	
 	
 	public String getMeilleureSolution(int i){
