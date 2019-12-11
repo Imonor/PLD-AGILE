b620617746c7ad5c5d9ea9ad0e9391502692d93d
Robert Cinciuc <rcinciuc@insa-lyon.fr>
2019-12-11 16:56:45 +0100

    add 2 CmdAjoutLivraisonTest

--- UberMims/src/controleur/CmdAjoutLivraisonTest.java
+++ UberMims/src/controleur/CmdAjoutLivraisonTest.java
@@ -2,8 +2,13 @@ package controleur;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import model.Chemin;
+import model.ContraintesTournee;
 import model.Intersection;
 import model.PointEnlevement;
 import model.PointLivraison;
@@ -15,7 +20,7 @@ import vue.Fenetre;
 public class CmdAjoutLivraisonTest {
 
 	@Test
-	public void testerAjoutLivraison() {
+	public void testerAjoutLivraisonNormal() {
 		
 		Fenetre fenetre = new Fenetre();
 		Controleur controleur = new Controleur();
@@ -44,13 +49,44 @@ public class CmdAjoutLivraisonTest {
 
 		controleur.ajouterLivraison(ptEnlev, ptLiv);
 		
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\");
 		System.out.print( tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getPremiere().getId() );
 		System.out.print( tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getDerniere().getId() );
 		
 		assertEquals( ptEnlev.getId(), tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getPremiere().getId());
-		assertEquals( ptLiv.getId(), tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getDerniere().getId());
+		assertEquals( ptLiv.getId(), tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getDerniere().getId());	
 		
-				
+		System.out.println("end");
 	}
-
+	
+	
+	@Test
+	public void testerAjoutLivraisonListeVide() {
+		
+		Controleur controleur = new Controleur();
+		
+		Intersection inter1 = new Intersection("55474924", 45.75965, 4.882271);
+		Intersection inter2 = new Intersection("60491706", 45.76259, 4.8819356);
+		Intersection depot = new Intersection("4150019167", 45.752956, 4.8982544);
+		
+		PointEnlevement ptEnlev = new PointEnlevement(inter1, "60491706", 10);
+		PointLivraison ptLiv = new PointLivraison(inter2, "55474924", 10);
+		
+		ContraintesTournee contraintes = new ContraintesTournee(LocalTime.now(), depot, new ArrayList<PointEnlevement>(), new ArrayList<PointLivraison>() );
+		Tournee tournee = new Tournee( new ArrayList<Chemin>(), contraintes );
+		controleur.setTournee(tournee);
+		
+		controleur.ajouterLivraison(ptEnlev, ptLiv);
+		
+		System.out.println("/////////////////////////");
+		for(int count = 0; count < tournee.getPlusCourteTournee().size(); ++count) {
+			System.out.print(count+"."+tournee.getPlusCourteTournee().get(count).getPremiere().getId()+"->"+tournee.getPlusCourteTournee().get(count).getDerniere().getId()+" | ");
+		}
+		
+//		System.out.print( tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getPremiere().getId() );
+//		System.out.print( tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getDerniere().getId() );
+//		
+		assertEquals( ptEnlev.getId(), tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getPremiere().getId());
+		assertEquals( ptLiv.getId(), tournee.getPlusCourteTournee().get(tournee.getPlusCourteTournee().size()-2).getDerniere().getId());	
+	}
 }
