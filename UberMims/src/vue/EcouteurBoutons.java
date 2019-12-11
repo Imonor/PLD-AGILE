58552db8dd130074fb9741059a08ece204134616
Imonor <ilan.piperno@gmail.com>
2019-12-11 15:31:39 +0100

    Fix heure passage

--- UberMims/src/vue/EcouteurBoutons.java
+++ UberMims/src/vue/EcouteurBoutons.java
@@ -162,7 +162,7 @@ public class EcouteurBoutons implements ActionListener{
 				fenetre.getAffichagePlan().setNouvelleLivraison(null);
 				fenetre.getAffichagePlan().setPlanClickable(false);
 				fenetre.afficherInfos();
-				fenetre.getAffichageTournee().afficherDetailTournee(fenetre.getTournee(),fenetre.getContraintes());
+				fenetre.getAffichageTournee().afficherDetailTournee(controleur.getTournee(),controleur.getContraintes());
 			break;
 			
 			case "Valider l'ajout d'une livraison":
@@ -189,7 +189,7 @@ public class EcouteurBoutons implements ActionListener{
 				fenetre.setTournee(controleur.getTournee());
 				fenetre.apresAjoutLivraison();
 				fenetre.afficherInfos();
-				fenetre.getAffichageTournee().afficherDetailTournee(fenetre.getTournee(),fenetre.getContraintes());
+				fenetre.getAffichageTournee().afficherDetailTournee(controleur.getTournee(),controleur.getContraintes());
 				
 			break;
 
